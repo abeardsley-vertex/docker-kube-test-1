88fcb2746c690ab80865127aa1bfcfcfd403b59d
Andrew Beardsley <abeardsley@vertex.com>
2021-05-19 17:12:56 -0500

    add kubernetes yamls

--- mvc-web-app/Startup.cs
+++ mvc-web-app/Startup.cs
@@ -56,6 +56,9 @@ namespace mvc_web_app
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.MigrateDatabaseToLatest();
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
